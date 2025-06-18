f6018c06e8c2a347e1a1a4b71adec66f73dec90f
notabird <notabird07@gmail.com>
2025-06-18 05:20:36 -0700

    kill me forgor to push this because of rider git plugin

--- BringBackBugBabies/Plugin Info.cs
+++ BringBackBugBabies/Plugin Info.cs
@@ -1,9 +1,5 @@
-namespace BringBackBugBabies
+namespace BringBackBugBabies;
+public class Plugin_Info
 {
-    public class Plugin_Info
-    {
-        public const string GUID = "NotABird.Bring.Back.Bug.Babies",
-                            NAME = "Bring Back Bug Babies",
-                            VERSION = "1.0.0";
-    }
+    public const string GUID = "NotABird.Bring.Back.Bug.Babies", NAME = "Bring Back Bug Babies", VERSION = "1.0.0";
 }
--- BringBackBugBabies/Plugin.cs
+++ BringBackBugBabies/Plugin.cs
@@ -1,25 +1,41 @@
-﻿using System.Reflection;
+﻿using System;
 using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
+using UnityEngine;
+using Random = UnityEngine.Random;
 
-namespace BringBackBugBabies
+namespace BringBackBugBabies;
+
+[BepInPlugin(Plugin_Info.GUID, Plugin_Info.NAME, Plugin_Info.VERSION)]
+public class Plugin : BaseUnityPlugin
 {
-    [BepInPlugin(Plugin_Info.GUID,  Plugin_Info.NAME, Plugin_Info.VERSION)]
-    public class Plugin : BaseUnityPlugin
+    private ConfigEntry<int> _amount;
+    public void Awake()
     {
-        public static Plugin Instance;
+        _amount = Config.Bind("Settings", "Amount", 5, "Amount of babies, min is 5");
+        GorillaTagger.OnPlayerSpawned(Init);
+    }
 
-        public void Awake()
+    private void Init()
+    {
+        if (_amount.Value < 5)
+            _amount.SetSerializedValue("5");
+        
+        GameObject babiesParent = GameObject.Find("Environment Objects/05Maze_PersistentObjects/BugBabies");//eww GameObject.Find
+        babiesParent.SetActive(true);
+
+        for (int i = babiesParent.transform.childCount; i < _amount.Value; i++)
         {
-            Instance = this;
-            new Harmony(Plugin_Info.GUID).PatchAll(Assembly.GetExecutingAssembly());
-
-            GorillaTagger.OnPlayerSpawned(Init);
-        }
-        //should run when the player spawns, but this is inconsistent
-        public void Init()
-        {
-
+            bool thing2 = Convert.ToBoolean(Random.Range(0, 2));//gets the blue or tan bug
+            
+            GameObject bug = Instantiate(babiesParent.transform.GetChild(thing2 ? 0 : 3).gameObject, babiesParent.transform, false);
+            bug.transform.GetChild(0).localPosition = new Vector3(0, 0, Random.Range(-1, 0));
+            
+            AccessTools.Field(typeof(PredicatableRandomRotation), "rot")
+                .SetValue(bug.GetComponent<PredicatableRandomRotation>(), RandomVector3(0.3f, -0.3f));
         }
     }
+
+    private Vector3 RandomVector3(float max, float min) => new Vector3(Random.Range(min, max), Random.Range(min, max), Random.Range(min, max));
 }
