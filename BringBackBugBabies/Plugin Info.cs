b5227c66fe8ff8f4842026e5b99793120962bcf6
notabird <notabird07@gmail.com>
2025-06-17 21:57:52 -0700

    Initial commit

---
+++ BringBackBugBabies/Plugin Info.cs
@@ -0,0 +1,9 @@
+namespace BringBackBugBabies
+{
+    public class Plugin_Info
+    {
+        public const string GUID = "NotABird.Bring.Back.Bug.Babies",
+                            NAME = "Bring Back Bug Babies",
+                            VERSION = "1.0.0";
+    }
+}
